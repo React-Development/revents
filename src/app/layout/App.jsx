dc305e5fcfef5716e084c109ff775db77d5b0964
Brayan Garcia <bgarcia.95@outlook.com>
2020-10-04 17:03:41 -0600

    Cleanup code. Import  and use EventDashboard component

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,16 +1,13 @@
 import React from "react";
-import { Button } from "semantic-ui-react";
+import EventDashboard from "../../features/events/eventsDashboard/EventDashboard";
 
-function App() {
+const App = () => {
   return (
-    <div className='App'>
+    <div>
       <h1>Re-vents</h1>
-      <button className='ui icon red button'>
-        <i className='user icon' /> CSS Button
-      </button>
-      <Button icon='user' content='React Button' color='green' loading={true} />
+      <EventDashboard />
     </div>
   );
-}
+};
 
 export default App;
