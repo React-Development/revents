71932837424e40d592c419c1d255256c3b11bb51
Brayan Garcia <bgarcia.95@outlook.com>
2020-10-04 17:33:10 -0600

    Add EventList basic structure

---
+++ src/features/events/eventsDashboard/EventList.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import EventListItem from "./EventListItem";
+
+const EventList = (props) => {
+  return (
+    <>
+      <EventListItem />
+      <EventListItem />
+      <EventListItem />
+      <EventListItem />
+      <EventListItem />
+    </>
+  );
+};
+
+export default EventList;
