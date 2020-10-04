96b52b87fef7d4abb659b0cb715042f20e962da1
Brayan Garcia <bgarcia.95@outlook.com>
2020-10-04 17:41:02 -0600

    Add EventForm component to EventDashboard

--- src/features/events/eventsDashboard/EventDashboard.jsx
+++ src/features/events/eventsDashboard/EventDashboard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Grid } from "semantic-ui-react";
+import EventForm from "../eventForm/EventForm";
 import EventList from "./EventList";
 
 const EventDashboard = () => {
@@ -9,7 +10,7 @@ const EventDashboard = () => {
         <EventList />
       </Grid.Column>
       <Grid.Column width={6}>
-        <h2>Right Column</h2>
+        <EventForm />
       </Grid.Column>
     </Grid>
   );
